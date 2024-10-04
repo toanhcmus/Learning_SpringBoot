ffc055b4eeb6abe24b0f5e95ed919c9f06194fe0
21120150 <toancva153@gmail.com>
2024-10-04 17:20:05 +0700

    SpringBoot Exception Handler and Validation

--- src/main/java/com/toanns/identity_service/dto/request/UserCreationRequest.java
+++ src/main/java/com/toanns/identity_service/dto/request/UserCreationRequest.java
@@ -1,9 +1,13 @@
 package com.toanns.identity_service.dto.request;
 
+import jakarta.validation.constraints.Size;
+
 import java.time.LocalDate;
 
 public class UserCreationRequest {
     private String username;
+
+    @Size(min = 8, message = "Password must be at least 8 characters.")
     private String password;
     private String firstName;
     private String lastName;
